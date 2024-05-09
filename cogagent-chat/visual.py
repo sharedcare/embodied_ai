88c8fd349f0d970cf0dfbfe9cdf0913a286419aa
ascend <sharedcare@hotmail.com>
2024-05-09 20:35:35 +0800

    Add configs and update implementations

--- cogagent-chat/visual.py
+++ cogagent-chat/visual.py
@@ -6,9 +6,31 @@ from argparse import Namespace
 from transformers.activations import ACT2FN
 
 
-def memory_efficient_attention():
-    # TODO
-    pass
+def memory_efficient_attention(query: mx.array, key: mx.array, value: mx.array, attn_bias=None, p=0., scale=None) -> mx.array:
+    """Refer to pytorch implementation of memory-efficient attention:
+    https://facebookresearch.github.io/xformers/components/ops.html#module-xformers.ops
+
+    """
+    if scale is None:
+        scale = 1.0 / query.shape[-1] ** 0.5
+
+    # (B, L, H, D) -> (B, H, L, D)
+    query = query * scale
+    query = query.transpose(1, 2)
+    key = key.transpose(1, 2)
+    value = value.transpose(1, 2)
+
+    # (B, H, L, D) @ (B, H, D, L) -> (B, H, L, L)
+    attn = query @ key.transpose(-2, -1)
+    if attn_bias is not None:
+        attn = attn + attn_bias
+    attn = mx.softmax(attn, axis=-1)
+    attn = nn.Dropout(p)(attn)
+    # (B, H, L, L) @ (B, H, L, D) -> (B, H, L, D)
+    attn = attn @ value
+    # (B, H, L, D) -> (B, L, H, D)
+    out = attn.transpose(1, 2)
+    return out
 
 
 class PatchEmbedding(nn.Module):
@@ -29,9 +51,9 @@ class PatchEmbedding(nn.Module):
         # (B, C, H, W) -> (B, L, D)
         x = self.proj(images)
         x = x.flatten(2).transpose(1, 2)
-        cls_token = self.cls_embedding.expand(x.shape[0], -1, -1)
+        cls_token = mx.broadcast_to(self.cls_embedding, [x.shape[0], -1, -1])
         x = mx.concatenate((cls_token, x), dim=1)
-        x += self.position_embedding.weight.unsqueeze(0)
+        x += mx.expand_dims(self.position_embedding.weight, axis=0)
         return x
 
 
@@ -51,23 +73,20 @@ class Attention(nn.Module):
         # (B, L, D) -> (B, L, D)
         B, L, _ = x.shape
         qkv = self.query_key_value(x)
-        qkv = qkv.reshape(B, L, 3, self.num_heads, -1).permute(
+        qkv = qkv.reshape(B, L, 3, self.num_heads, -1).transpose(
             2, 0, 1, 3, 4
         )  # 3, B, L, H, D
         q, k, v = qkv[0], qkv[1], qkv[2]
         out = memory_efficient_attention(
-            q,
-            k,
-            v,
-            scale=self.scale,
+            q, k, v, scale=self.scale,
         )
-        output = self.dense(out.view(B, L, -1))
+        output = self.dense(out.reshape(B, L, -1))
         output = self.output_dropout(output)
         return output
 
     def attention(self, q: mx.array, k: mx.array, v: mx.array) -> mx.array:
         attn_weights = q * self.scale @ k.transpose(-2, -1)
-        attn_weights = attn_weights.softmax(dim=-1)
+        attn_weights = mx.softmax(attn_weights, axis=-1)
         output = attn_weights @ v
         return output
 
@@ -80,7 +99,7 @@ class MLP(nn.Module):
         self.fc1 = nn.Linear(config.hidden_size, config.intermediate_size)
         self.fc2 = nn.Linear(config.intermediate_size, config.hidden_size)
 
-    def forward(self, x: mx.array) -> mx.array:
+    def __call__(self, x: mx.array) -> mx.array:
         x = self.fc1(x)
         x = self.activation_fn(x)
         x = self.fc2(x)
@@ -99,7 +118,7 @@ class TransformerLayer(nn.Module):
             config.hidden_size, eps=config.layer_norm_eps
         )
 
-    def forward(self, hidden_states: mx.array) -> mx.array:
+    def __call__(self, hidden_states: mx.array) -> mx.array:
         attention_input = hidden_states
         attention_output = self.input_layernorm(self.attention(attention_input))
         hidden_states = attention_input + attention_output
@@ -114,7 +133,7 @@ class Transformer(nn.Module):
         super().__init__()
         self.layers = [TransformerLayer(config) for _ in range(config.num_hidden_layers)]
 
-    def forward(self, hidden_states: mx.array) -> mx.array:
+    def __call__(self, hidden_states: mx.array) -> mx.array:
         for layer_module in self.layers:
             hidden_states = layer_module(hidden_states)
         return hidden_states
@@ -125,8 +144,8 @@ class GLU(nn.Module):
         super().__init__()
         self.linear_proj = nn.Linear(in_features, config.hidden_size, bias=False)
         self.norm1 = nn.LayerNorm(config.hidden_size)
-        self.act1 = nn.GELU()
-        self.act2 = nn.functional.silu
+        self.act1 = nn.gelu
+        self.act2 = nn.silu
         self.dense_h_to_4h = nn.Linear(
             config.hidden_size, config.intermediate_size, bias=False
         )
@@ -137,7 +156,7 @@ class GLU(nn.Module):
             config.intermediate_size, config.hidden_size, bias=False
         )
 
-    def forward(self, x):
+    def __call__(self, x):
         x = self.linear_proj(x)
         x = self.act1(self.norm1(x))
         x = self.act2(self.gate_proj(x)) * self.dense_h_to_4h(x)
@@ -159,13 +178,13 @@ class EVA2CLIPModel(nn.Module):
             vision_config.hidden_size],
         )
 
-    def forward(self, images: mx.array) -> mx.array:
+    def __call__(self, images: mx.array) -> mx.array:
         # (B, C, H, W) -> (B, L, D)
         x = self.patch_embedding(images)
         x = self.transformer(x)
         x = x[:, 1:]
-        x = self.linear_proj(x + self.pos_embed.to(x.device).unsqueeze(0))
-        boi = self.boi.to(x.device).expand(x.shape[0], -1, -1)
-        eoi = self.eoi.to(x.device).expand(x.shape[0], -1, -1)
+        x = self.linear_proj(x + mx.expand_dims(self.pos_embed, axis=0))
+        boi = mx.broadcast_to(self.boi, [x.shape[0], -1, -1])
+        eoi = mx.broadcast_to(self.eoi, [x.shape[0], -1, -1])
         x = mx.concatenate((boi, x, eoi), axis=1)
         return x
